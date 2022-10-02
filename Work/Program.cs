718747f6d40f133ea1bd0543cbbd3a147fc8a79e
Wulkain <zoombievi@gmail.com>
2022-10-02 21:46:14 +0300

    AddToDiagramm+Programm

--- Work/Program.cs
+++ Work/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine();
+System.Console.WriteLine($"Изначальный массив к обработке: 123 23 hello world res");
+string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array2 = new string[array1.Length];
+void ArrayOfSort(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ArrayOfSort(array1, array2);
+System.Console.WriteLine();
+System.Console.Write("Массив из не более 3х элементов в ячейке:");
+PrintArray(array2);
+System.Console.WriteLine();
